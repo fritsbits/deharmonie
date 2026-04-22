68e2396370170019bf01ac248c50ff07f9240dd3
Frederik Vincx <frederik.vincx@gmail.com>
2026-04-22 16:44:37 +0200

    test(admin): weak new password blocks change

--- tests/Feature/Filament/EditProfileTest.php
+++ tests/Feature/Filament/EditProfileTest.php
@@ -76,4 +76,25 @@ class EditProfileTest extends TestCase
 
         $this->assertSame($originalHash, $admin->refresh()->password);
     }
+
+    public function test_weak_new_password_blocks_change(): void
+    {
+        $this->seed(AdminUserSeeder::class);
+        $admin = $this->adminUser();
+        $admin->password = Hash::make('old-password-123');
+        $admin->save();
+        $originalHash = $admin->password;
+
+        Livewire::actingAs($admin)
+            ->test(EditProfile::class)
+            ->fillForm([
+                'currentPassword' => 'old-password-123',
+                'password' => 'short',
+                'passwordConfirmation' => 'short',
+            ])
+            ->call('save')
+            ->assertHasFormErrors(['password']);
+
+        $this->assertSame($originalHash, $admin->refresh()->password);
+    }
 }
